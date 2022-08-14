cdcde145e235446ff18ef9feba954f7e3ceaa077
Uldarenc1y <111232678+Uldarenc1y@users.noreply.github.com>
2022-08-14 17:55:54 +0400

    Add files via upload
    строки

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
-
-main()
+#define IN 0
+#define OUT 1
+int main()
 {
-    int c;
-    while ((c = getchar()) != '\b'){
-        printf("%d", c);
-
-    }
-        printf("%d", c);
-
+    int c, nc, nw, nl, test;
+    nc = nw = nl = 0;
+    test = OUT;
+    while ((c = getchar()) != EOF)
+    {nc++;
+    if (c == '\n')
+        {nl++;}
+    else if ((c == '\t' || c == ' ' || c == '\n') && test != OUT)
+        {nw++;
+        test = OUT;}
+    else if (test == OUT){
+        test = IN;
+        }}
+    printf("%d\n%d\n%d", nc, nw, nl);
+    return 0;
 }
-
-
-
